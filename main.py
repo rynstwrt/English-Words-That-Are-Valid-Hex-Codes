88c00085f0c2a2da119d4dd96e2db5727679e316
Ryan Stewart <ryanstewartalex@gmail.com>
2023-07-09 15:30:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from english_words import get_english_words_set
 import re
 
-
 words = get_english_words_set(["web2", "gcide"], lower=True)
 pattern = re.compile(r"[a-fA-F0-9]{6}")
-
 
 for word in words:
     if len(word) == 6 and pattern.match(word):
